82e1e9f3948bd3b7006af4a9eb2142ef9566b71a
KarthickTechie <reachkarthickprogramer@gmail.com>
2023-09-11 09:47:35 +0530

    added comment

--- www/WebpMaker.js
+++ www/WebpMaker.js
@@ -3,7 +3,7 @@ var exec = require("cordova/exec");
 module.exports.coolMethod = function (arg0, success, error) {
   exec(success, error, "WebpMaker", "coolMethod", [arg0]);
 };
-
+/* this method add two numbers */
 module.exports.add = function (arg0, success, error) {
   exec(success, error, "WebpMaker", "add", [arg0]);
 };
